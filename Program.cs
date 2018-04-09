3e179f03d37328ca34a41783f751e357d470f2a3
fraser-in-school <37731322+fraser-in-school@users.noreply.github.com>
2018-04-09 16:01:10 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace ConsoleApp1
 {   public class Suduku
@@ -53,22 +53,34 @@ namespace ConsoleApp1
             
             return 0;/*未发现错误*/
         }
-        public static void Write_txt(int[,] ans)
+        public static void Write_txt(int[,] ans,int k)
         {
-            StreamWriter sw = new StreamWriter("out.txt", true, Encoding.Default);
-            sw.WriteLine("{0}",Count.count);
+            /*FileStream fw = new FileStream("out.txt", FileMode.Create,FileAccess.Write);
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            binaryFormatter.Serialize(fw, ans);
+            fw.Dispose();
             for(int i=0;i<9;i++)
             {
                 
                 for (int j = 0; j < 9; j++)
                 {
-                    sw.Write(ans[i, j].ToString() + " ");
+                    fw.Write(ans[i, j].ToString() + " ");sw.Flush();
                 }
-                sw.WriteLine();
+                fw.WriteLine();
+            }*/
+            File.sw.WriteLine("{0}", k);
+            for (int i = 0; i < 9; i++)
+            {
+
+                for (int j = 0; j < 9; j++)
+                {
+                    File.sw.Write(ans[i, j].ToString() + " ");
+                    //sw.Dispose();
+                }
+                File.sw.WriteLine();
             }
-            sw.Flush();
-            sw.Close();
-                
+            //sw.Close();
+
         }
         public static void Exchange(int[,] ans,int before,int after,bool Is_line)
         {
@@ -109,7 +121,7 @@ namespace ConsoleApp1
                     for (int j = 0; j < 6; j++)
                     {
                         Suduku.Exchange(ans, row[0, j], row[1, j], false);
-                        Write_txt(ans);
+                        //Write_txt(ans,count);
                         Count.count++;
                        // Console.WriteLine("{0}",Count.count);
                     }
@@ -137,34 +149,75 @@ namespace ConsoleApp1
                 }
             }
         }
-    public void Create_suduku(int k)
+        public static void Swap(int[] sample,int i,int j)
+        {
+            int temp = sample[i];
+            sample[i] = sample[j];
+            sample[j] = temp;
+        }
+        public static void Create_suduku(int k)
         {
             int[,] ans = new int[9, 9];//ans是当前的数独方案
             int count = 0;
-            
             ans[0, 0] = 4;
-            while (count < k)
-            {
-                int[] sample = new int[9];
+
+                int[] sample = { 4, 1, 2, 3, 5, 6, 7, 8, 9 };
                 sample[0] = 4;
-                Dfs(1, sample);
-                
+                for(int i=1;i<9;i++)
+                {
+                    for(int j=i;j<9;j++)
+                    {
+                        Suduku.Swap(sample, i, j);
+                        foreach (int temp in sample)
+                            Console.Write("{0} ",temp);
+                        Console.WriteLine();
+                        //Console.ReadKey();
+                        int[] shift = { 0, 3, 6, 7, 4, 1, 2, 5, 8 };
+                        for (int p = 0; p < 9; p++)
+                        {
+                            for (int q = 0; q < 9; q++)
+                            {
+                                ans[p, q] = sample[(q + shift[p]) % 9];
+                            }
+                        }
+                        int[,] line = { { 3, 4, 5, 6, 7, 8 }, { 5, 3, 4, 8, 6, 7 } };
+                        int[,] row = { { 0, 3, 4, 5, 6, 7, 8 }, { 0, 5, 3, 4, 8, 6, 7 } };
+                        for (int p = 0; p < 6; p++)
+                        {
+                            if (count > k) return;
+                            Suduku.Exchange(ans, line[0, p], line[1, p], true);
+                            for (int q = 0; q < 6; q++)
+                            {
+                                Suduku.Exchange(ans, row[0, q], row[1, q], false);
+                                Write_txt(ans,count);
+                                count++;
+                                // Console.WriteLine("{0}",Count.count);
+                            }
+                        }
+                    }
+                }
                
-            }
+            
             
             
         }
-    }
-    public class  Count
+   }
+    public class  File
     {
-        public static int count=0;
+        public static StreamWriter sw = new StreamWriter("out.txt",false, Encoding.Default);
+    }
+    public class Count
+    {
+        public static int count = 0;
     }
     class Program
     {
         static void Main(string[] args)
         {
             int[] Line = new int[9];
+            
             Line[0] = 4;
+            //Suduku.Create_suduku(1000000);
             Suduku.Dfs(1,Line);
         }
     }
